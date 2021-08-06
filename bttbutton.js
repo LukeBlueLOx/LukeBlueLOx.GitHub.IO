9ba13b8a735100b858d2e62a8912cdd71520f08e
LukeBlueLOx <86468152+LukeBlueLOx@users.noreply.github.com>
2021-08-06 13:29:36 +0200

    Update bttbutton.js

--- bttbutton.js
+++ bttbutton.js
@@ -1,7 +1,7 @@
-var btn = $('#bttbutton');
+var btn = $('#button');
 
 $(window).scroll(function() {
-  if ($(window).scrollTop() > 1000) {
+  if ($(window).scrollTop() > 300) {
     btn.addClass('show');
   } else {
     btn.removeClass('show');
